2f2fb29a8713f088c43256f990283526d55cc163
Mai Tien Thanh <abc@gmai.com>
2026-03-30 22:14:01 +0700

    Update blog posts

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/logo1.svg',
       colorMode: {
         respectPrefersColorScheme: true,
       },
@@ -69,7 +69,7 @@ const config = {
         title: 'Tien Thanh Blog',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo1.svg',
         },
         items: [
           {
